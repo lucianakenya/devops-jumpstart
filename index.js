0eec033be2f72856a635cb98878349545b90091b
Ubuntu <ubuntu@ip-172-31-12-40.eu-north-1.compute.internal>
2025-11-11 10:55:48 +0000

    Trigger workflow

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-  res.send('🚀 Welcome to your DevOps Jumpstart Node.js App!');
+  res.send('🚀 Welcome to your DevOps Jumpstart Node.js App,You did good!');
 });
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
